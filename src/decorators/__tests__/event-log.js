6f65ea044a0a3d1c9f9f0f598f87d303b9117624
Zhenyu Lin <zhenyu.lyn@gmail.com>
2020-03-04 18:06:24 +0000

    refactor(decorators/event-log): using addHooks

--- src/decorators/__tests__/event-log.js
+++ src/decorators/__tests__/event-log.js
@@ -10,103 +10,67 @@ describe('eventLog', () => {
     jest.clearAllMocks();
   });
 
-  describe('when logger not present in context', () => {
-    it('skip the log and return the result in await', async () => {
-      const original = () => 'yes';
-      const decorated = eventLog()(original);
-      const result = await decorated();
+  it('log the success event with param when enabled', async () => {
+    const original = async ({ text }) => text;
+    const decorated = eventLog({ logParam: true })(original);
+    const result = await decorated({ text: 'yes' }, {}, { logger });
 
-      expect(result).toBe('yes');
-      expect(logger.info.mock.calls.length).toBe(0);
-    });
-
-    it('throws error from the original function', async () => {
-      const original = () => {
-        const e = { message: 'error' };
-        throw e;
-      };
-      const decorated = eventLog()(original);
-
-      try {
-        await decorated();
-      } catch (e) {
-        expect(e.message).toBe('error');
-      }
-    });
+    expect(result).toBe('yes');
+    expect(logger.info.mock.calls).toMatchSnapshot();
   });
 
-  describe('when logger present in context', () => {
-    it('log the event when input function call succeeds', async () => {
-      const original = async () => 'yes';
-      const decorated = eventLog()(original);
-      const result = await decorated({}, {}, { logger });
+  it('log the success event with result when enabled', async () => {
+    const original = async () => 'yes';
+    const decorated = eventLog({ logResult: true })(original);
+    const result = await decorated({}, {}, { logger });
 
-      expect(result).toBe('yes');
+    expect(result).toBe('yes');
+    expect(logger.info.mock.calls).toMatchSnapshot();
+  });
+
+  it('log chained events from upper level function call', async () => {
+    const original = () => {};
+    const upper = (p, m, c) => {
+      return eventLog()(original)(p, m, c);
+    };
+    await eventLog()(upper)({}, {}, { logger });
+    expect(logger.info.mock.calls).toMatchSnapshot();
+  });
+
+  it('log the error event when input function fails', async () => {
+    const original = () => {
+      const e = { message: 'error' };
+      throw e;
+    };
+    const decorated = eventLog()(original);
+
+    try {
+      await decorated({}, {}, { logger });
+    } catch (e) {
+      expect(e.message).toBe('error');
       expect(logger.info.mock.calls).toMatchSnapshot();
-    });
+      expect(logger.error.mock.calls).toMatchSnapshot();
+    }
+  });
 
-    it('log the success event with param when enabled', async () => {
-      const original = async ({ text }) => text;
-      const decorated = eventLog({ includeParam: true })(original);
-      const result = await decorated({ text: 'yes' }, {}, { logger });
+  it('log the parsed error when errorParse set', async () => {
+    const original = () => {
+      const e = new Error('error');
+      throw e;
+    };
+    const decorated = eventLog({
+      errorParser: e => ({
+        message: e.message,
+        stack: e.stack.substring(0, 20),
+      }),
+    })(original);
 
-      expect(result).toBe('yes');
+    try {
+      await decorated({}, {}, { logger });
+    } catch (e) {
+      expect(e.message).toBe('error');
       expect(logger.info.mock.calls).toMatchSnapshot();
-    });
-
-    it('log the success event with result when enabled', async () => {
-      const original = async () => 'yes';
-      const decorated = eventLog({ includeResult: true })(original);
-      const result = await decorated({}, {}, { logger });
-
-      expect(result).toBe('yes');
-      expect(logger.info.mock.calls).toMatchSnapshot();
-    });
-
-    it('log chained events from upper level function call', async () => {
-      const original = () => {};
-      const upper = (p, m, c) => {
-        return eventLog()(original)(p, m, c);
-      };
-      await eventLog()(upper)({}, {}, { logger });
-      expect(logger.info.mock.calls).toMatchSnapshot();
-    });
-
-    it('log the error event when input function fails', async () => {
-      const original = () => {
-        const e = { message: 'error' };
-        throw e;
-      };
-      const decorated = eventLog()(original);
-
-      try {
-        await decorated({}, {}, { logger });
-      } catch (e) {
-        expect(e.message).toBe('error');
-        expect(logger.info.mock.calls).toMatchSnapshot();
-        expect(logger.error.mock.calls).toMatchSnapshot();
-      }
-    });
-
-    it('log the parsed error when errorParse set', async () => {
-      const original = () => {
-        const e = new Error('error');
-        throw e;
-      };
-      const decorated = eventLog({
-        errorParse: e => ({
-          message: e.message,
-          stack: e.stack.substring(0, 20),
-        }),
-      })(original);
-
-      try {
-        await decorated({}, {}, { logger });
-      } catch (e) {
-        expect(e.message).toBe('error');
-        expect(logger.info.mock.calls).toMatchSnapshot();
-        expect(logger.error.mock.calls).toMatchSnapshot();
-      }
-    });
+      expect(logger.error.mock.calls).toMatchSnapshot();
+    }
   });
 });
--- src/decorators/event-log.js
+++ src/decorators/event-log.js
@@ -1,3 +1,5 @@
+import addHooks from './helpers/add-hooks';
+
 /*
   a decorator to attach standard log behaviour to action
   bypass: when logger instance is not available in context
@@ -5,39 +7,27 @@
   after: log success event, with options to include param and result
   error: log error event, with option to parse error
  */
-// note: it is simpler to implement this without addHooks helper
-const eventLog = ({
-  includeParam,
-  includeResult,
-  errorParse,
-} = {}) => action => async (param, meta = {}, context = {}) => {
-  const { logger } = context;
-  if (!logger) return action(param, meta, context);
-
-  const { name } = action;
-  const event = meta.event ? `${meta.event}.${name}` : name;
-
-  try {
-    const result = await action(param, { ...meta, event }, context);
-
-    const log = {
-      ...meta,
-      event,
-      ...(includeParam ? { param } : {}),
-      ...(includeResult ? { result } : {}),
-    };
-    logger.info(log, event);
-
-    return result;
-  } catch (e) {
-    // e.stack can be parsed via errorParse
-    logger.error(
-      { ...meta, event, error: errorParse ? errorParse(e) : e },
-      e.message,
-    );
-
-    throw e;
-  }
-};
+const eventLog = ({ logParam, logResult, errorParser = e => e } = {}) =>
+  addHooks({
+    bypassHook: (p, m, c) => !c.logger,
+    storeHook: (p, meta, c, action) => {
+      const { name } = action;
+      const event = meta.event ? `${meta.event}.${name}` : name;
+      return { event };
+    },
+    actionHook: (p, meta, c, a, { event }) => a(p, { ...meta, event }, c),
+    afterHook: (result, param, meta, { logger }, a, { event }) => {
+      const log = {
+        ...meta,
+        event,
+        ...(logParam ? { param } : {}),
+        ...(logResult ? { result } : {}),
+      };
+      logger.info(log, event);
+    },
+    errorHook: (e, p, meta, { logger }, a, { event }) => {
+      logger.error({ ...meta, event, error: errorParser(e) }, e.message);
+    },
+  });
 
 export default eventLog;
